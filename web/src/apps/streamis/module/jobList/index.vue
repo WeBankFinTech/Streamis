10c3827c6297b2c58b836277460b8e2fc120ebaf
司海洋 <sihy@chinatelecom.cn>
2021-04-09 16:44:19 +0800

    init front-end of real-time job center

---
+++ web/src/apps/streamis/module/jobList/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <titleCard :title="$t('message.streamis.moduleName.jobList')">
+      <div>
+      </div>
+    </titleCard>
+  </div>
+</template>
+<script>
+import api from "@/common/service/api";
+import titleCard from '@/apps/streamis/components/titleCard';
+export default {
+  components: { titleCard },
+  data() {
+    return {
+      indexItems: [
+        { name: "failture", num: 0, icon: "md-close-circle", color: "#990033" },
+        { name: "running", num: 0, icon: "md-pint", color: "#008000" },
+        { name: "slowTask", num: 0, icon: "md-help-circle", color: "#6666FF" },
+        { name: "alert", num: 0, icon: "md-warning", color: "#FF99CC" },
+        { name: "waitRestart", num: 0, icon: "md-alert", color: "#FF00CC" },
+        {
+          name: "success",
+          num: 0,
+          icon: "md-checkmark-circle",
+          color: "#008000"
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.getIndexData();
+  },
+  methods: {
+    getIndexData() {
+      api
+        .fetch(
+          "api/rest_j/v1/streamis/streamJobManager/project/core/target",
+          "get"
+        )
+        .then(res => {
+          console.log(res);
+        }).catch(e => console.log(e));
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
