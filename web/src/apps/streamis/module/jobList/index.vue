b733924efb18fbed5d914b093c634445eba2b196
司海洋 <sihy@chinatelecom.cn>
2021-04-14 11:01:12 +0800

    init the task detail of real-time job center

--- web/src/apps/streamis/module/jobList/index.vue
+++ web/src/apps/streamis/module/jobList/index.vue
@@ -48,24 +48,35 @@
             </FormItem>
 
             <FormItem>
-              <Button type="primary" @click="handleQuery()" style="margin-left:80px">
+              <Button
+                type="primary"
+                @click="handleQuery()"
+                style="margin-left:80px"
+              >
                 {{ $t("message.streamis.formItems.queryBtn") }}
               </Button>
             </FormItem>
           </Form>
           <Table :columns="columns" :data="tableDatas">
-            <template slot-scope="{ row }" slot="jobName">
-              <div class="jobName">
+            <template slot-scope="{ row, index }" slot="jobName">
+              <div class="jobName" v-show="index === 0">
+                <Upload action="uuu" format="['jar']">
+                  <Icon type="md-add" class="upload" />
+                  <span>{{
+                    $t("message.streamis.jobListTableColumns.upload")
+                  }}</span>
+                </Upload>
+              </div>
+              <div class="jobName" v-show="index !== 0">
                 <Dropdown transfer @on-click="name => handleRouter(row, name)">
                   <Icon type="md-more" class="more" />
                   <DropdownMenu slot="list">
                     <DropdownItem
                       v-for="(item, index) in jobMoudleRouter"
                       :name="item"
-                      :key="index">
-                      {{
-                        $t("message.streamis.jobMoudleRouter." + item)
-                      }}
+                      :key="index"
+                    >
+                      {{ $t("message.streamis.jobMoudleRouter." + item) }}
                     </DropdownItem>
                   </DropdownMenu>
                 </Dropdown>
@@ -74,34 +85,43 @@
                 </div>
               </div>
             </template>
-            <template slot-scope="{ row }" slot="operation">
-              <Button
-                :type="row.taskStatus !== 'running' ? 'success' : 'error'"
-                size="small"
-                style="margin-right: 5px"
-                @click="handleAction(row)">
-                {{
-                  row.taskStatus !== "running"
-                    ? $t("message.streamis.formItems.startBtn")
-                    : $t("message.streamis.formItems.stopBtn")
-                }}
-              </Button>
-              <Button type="primary" size="small" @click="handleConfig(row)">
-                {{
-                  $t("message.streamis.formItems.configBtn")
-                }}
-              </Button>
-              <Button
-                type="success"
-                size="small"
-                style="margin-right: 5px"
-                v-show="row.taskStatus !== 'running'"
-                @click="handleAction(row)">
-                checkpoint
-              </Button>
+            <template slot-scope="{ row, index }" slot="operation">
+              <div v-show="index !== 0">
+                <Button
+                  :type="row.taskStatus !== 'running' ? 'success' : 'error'"
+                  size="small"
+                  style="margin-right: 5px"
+                  @click="handleAction(row)"
+                >
+                  {{
+                    row.taskStatus !== "running"
+                      ? $t("message.streamis.formItems.startBtn")
+                      : $t("message.streamis.formItems.stopBtn")
+                  }}
+                </Button>
+                <Button type="primary" size="small" @click="handleConfig(row)">
+                  {{ $t("message.streamis.formItems.configBtn") }}
+                </Button>
+                <Button
+                  type="success"
+                  size="small"
+                  style="margin-right: 5px"
+                  v-show="row.taskStatus !== 'running'"
+                  @click="handleAction(row)"
+                >
+                  checkpoint
+                </Button>
+              </div>
             </template>
           </Table>
-          <Page :total="pageData.total" class="page" :page-size="pageData.pageSize" show-total show-elevator show-sizer/>
+          <Page
+            :total="pageData.total"
+            class="page"
+            :page-size="pageData.pageSize"
+            show-total
+            show-elevator
+            show-sizer
+          />
         </div>
       </div>
     </titleCard>
@@ -130,7 +150,7 @@ export default {
   data() {
     return {
       query: {
-        jobName: "safdsafsa",
+        jobName: "",
         jobStatus: "all",
         submitter: "all"
       },
@@ -193,7 +213,8 @@ export default {
               "span",
               {
                 style: {
-                  background: "rgba(204, 204, 255, 1)",
+                  background:
+                    params.index === 0 ? "fff" : "rgba(204, 204, 255, 1)",
                   display: "inline-block",
                   padding: "2px"
                 }
@@ -210,7 +231,7 @@ export default {
               "span",
               {
                 style: {
-                  background: "#008000",
+                  background: params.index === 0 ? "fff" : "#008000",
                   color: "#fff",
                   display: "inline-block",
                   padding: "2px"
@@ -244,7 +265,7 @@ export default {
       pageData: {
         total: 200,
         current: 1,
-        pageSize: 20,
+        pageSize: 20
       }
     };
   },
@@ -253,7 +274,7 @@ export default {
   },
   methods: {
     getJobList() {
-      this.tableDatas = new Array(10).fill(
+      const datas = new Array(10).fill(
         {
           taskId: "ipsum laboris ut proiden",
           jobName: "in",
@@ -267,6 +288,9 @@ export default {
         0,
         30
       );
+      datas.unshift({});
+      this.tableDatas = datas;
+
       api
         .fetch("api/rest_j/v1/streamis/streamJobManager/job/list", "get")
         .then(res => {
@@ -274,30 +298,53 @@ export default {
         })
         .catch(e => console.log(e));
     },
-    handleNameQuery(){
+    handleNameQuery() {
       console.log(this.query.jobName);
     },
     handleQuery() {},
-    handleAction(data) {console.log(data)},
-    handleConfig(data) {console.log(data)},
-    handleRouter(rowData, moudleName) {
+    handleAction(data) {
+      console.log(data);
+    },
+    handleConfig(data) {
+      console.log(data);
+    },
+    handleRouter(rowData, moduleName) {
       console.log(rowData);
-      console.log(moudleName);
+      console.log(moduleName);
+      const moduleMap = {
+        paramsConfiguration: "jobConfigure",
+        alertConfiguration: "jobConfigure",
+        runningHistory: "jobHistory"
+      };
+      this.$router.push({
+        name: "JobDetail",
+        params: {
+          id: rowData.taskId.replace(/\s/g, ""),
+          module: moduleName
+            ? moduleMap[moduleName] || moduleName
+            : "jobSummary",
+          name: rowData.jobName
+        }
+      });
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-.select{
+.select {
   width: 200px;
 }
 .jobName {
   display: flex;
+  cursor: pointer;
 }
 .more {
   font-size: 20px;
 }
-.page{
+.upload {
+  font-size: 24px;
+}
+.page {
   margin-top: 20px;
 }
 </style>
