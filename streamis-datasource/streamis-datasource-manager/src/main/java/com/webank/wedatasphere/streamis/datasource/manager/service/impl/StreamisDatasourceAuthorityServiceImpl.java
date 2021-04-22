d5fae15802ea7f410607590ed92eedb1d6f403b6
liuchengguo 00679 <liuchengguo@smyfinancial.com>
2021-04-22 10:38:41 +0800

    Add: datasource code first commit

---
+++ streamis-datasource/streamis-datasource-manager/src/main/java/com/webank/wedatasphere/streamis/datasource/manager/service/impl/StreamisDatasourceAuthorityServiceImpl.java
@@ -0,0 +1,21 @@
+package com.webank.wedatasphere.streamis.datasource.manager.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.webank.wedatasphere.streamis.datasource.manager.dao.StreamisDatasourceAuthorityDao;
+import com.webank.wedatasphere.streamis.datasource.manager.domain.StreamisDatasourceAuthority;
+import com.webank.wedatasphere.streamis.datasource.manager.service.StreamisDatasourceAuthorityService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * <p>
+ * 数据源授权表 服务实现类
+ * </p>
+ *
+ * @author c01013
+ * @since 2021-04-02
+ */
+@Service
+public class StreamisDatasourceAuthorityServiceImpl extends ServiceImpl<StreamisDatasourceAuthorityDao, StreamisDatasourceAuthority> implements StreamisDatasourceAuthorityService {
+
+}
