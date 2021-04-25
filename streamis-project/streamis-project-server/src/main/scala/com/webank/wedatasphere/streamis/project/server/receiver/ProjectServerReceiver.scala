dfbe2ea41b14b1f1fd59847cd6fdaa684761f245
yangzhiyue <904666286@qq.com>
2021-04-25 13:11:20 +0800

    stash to pull codes from github

---
+++ streamis-project/streamis-project-server/src/main/scala/com/webank/wedatasphere/streamis/project/server/receiver/ProjectServerReceiver.scala
@@ -0,0 +1,9 @@
+package com.webank.wedatasphere.streamis.project.server.receiver
+
+/**
+ * created by yangzhiyue on 2021/4/23
+ * Description:
+ */
+class ProjectServerReceiver extends Receiver{
+
+}
