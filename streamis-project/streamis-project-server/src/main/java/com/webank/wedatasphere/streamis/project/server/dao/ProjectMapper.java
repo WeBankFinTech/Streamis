f157688b91b87c1da9ff49b94eeb0d4d5185c5a6
yangzhiyue <904666286@qq.com>
2021-04-25 13:11:20 +0800

    stash to pull codes from github

---
+++ streamis-project/streamis-project-server/src/main/java/com/webank/wedatasphere/streamis/project/server/dao/ProjectMapper.java
@@ -0,0 +1,17 @@
+package com.webank.wedatasphere.streamis.project.server.dao;
+
+import com.webank.wedatasphere.streamis.project.server.entity.StreamisProject;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * created by yangzhiyue on 2021/4/23
+ * Description:
+ */
+@Mapper
+public interface ProjectMapper {
+
+    Long insertProject(StreamisProject streamisProject);
+
+
+
+}
