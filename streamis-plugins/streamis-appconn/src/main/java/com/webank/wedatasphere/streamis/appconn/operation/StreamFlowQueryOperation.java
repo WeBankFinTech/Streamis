fcab3891d648a0a5d1ae19f2c7bfed954ed9a305
yangzhiyue <904666286@qq.com>
2021-04-25 22:16:12 +0800

    commit some server and appconn codes

--- streamis-plugins/streamis-appconn/src/main/java/com/webank/wedatasphere/streamis/appconn/operation/StreamFlowQueryOperation.java
+++ streamis-plugins/streamis-appconn/src/main/java/com/webank/wedatasphere/streamis/appconn/operation/StreamFlowQueryOperation.java
@@ -2,9 +2,12 @@ package com.webank.wedatasphere.streamis.appconn.operation;
 
 import com.webank.wedatasphere.dss.standard.app.development.DevelopmentService;
 import com.webank.wedatasphere.dss.standard.app.development.query.RefQueryOperation;
+import com.webank.wedatasphere.dss.standard.common.desc.AppInstance;
 import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
+import com.webank.wedatasphere.linkis.rpc.Sender;
 import com.webank.wedatasphere.streamis.appconn.ref.StreamFlowQueryRequestRef;
 import com.webank.wedatasphere.streamis.appconn.ref.StreamFlowQueryResponseRef;
+import com.webank.wedatasphere.streamis.plugins.common.StreamisPluginUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +19,21 @@ public class StreamFlowQueryOperation implements RefQueryOperation<StreamFlowQue
 
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StreamFlowQueryOperation.class);
+
+    private DevelopmentService developmentService;
+
+    private Sender sender;
+
+    private AppInstance appInstance;
+
+    public StreamFlowQueryOperation(AppInstance appInstance){
+        this.appInstance = appInstance;
+        init();
+    }
+
+    private void init(){
+        this.sender = Sender.getSender(StreamisPluginUtils.STREAMIS_RPC_SERVER_NAME + "-" + StreamisPluginUtils.getLabel(appInstance));
+    }
 
 
 
--- streamis-workflow/streamis-workflow-server/src/main/java/com/webank/wedatasphere/streamis/workflow/server/exception/StreamisFlowErrorException.java
+++ streamis-workflow/streamis-workflow-server/src/main/java/com/webank/wedatasphere/streamis/workflow/server/exception/StreamisFlowErrorException.java
@@ -6,17 +6,17 @@ import com.webank.wedatasphere.linkis.common.exception.ErrorException;
  * created by yangzhiyue on 2021/4/19
  * Description:
  */
-public class StreamisErrorException extends ErrorException {
+public class StreamisFlowErrorException extends ErrorException {
 
 
 
 
 
-    public StreamisErrorException(int errCode, String desc) {
+    public StreamisFlowErrorException(int errCode, String desc) {
         super(errCode, desc);
     }
 
-    public StreamisErrorException(int errCode, String desc, Throwable t){
+    public StreamisFlowErrorException(int errCode, String desc, Throwable t){
         super(errCode, desc);
         this.initCause(t);
     }
