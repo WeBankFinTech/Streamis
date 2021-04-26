02e068d7c4537ebb4668a6bfcea17391dde37a83
Dlimeng <77954309@qq.com>
2021-04-26 11:09:37 +0800

    jobmanager all query

---
+++ streamis-jobmanager/streamis-job-manager/streamis-job-manager-service/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/manager/service/ProjectService.scala
@@ -0,0 +1,12 @@
+package com.webank.wedatasphere.streamis.jobmanager.manager.service
+
+import com.webank.wedatasphere.linkis.common.utils.Logging
+import org.springframework.stereotype.Service
+
+/**
+ * @author limeng
+ */
+@Service
+class ProjectService extends Logging{
+
+}
