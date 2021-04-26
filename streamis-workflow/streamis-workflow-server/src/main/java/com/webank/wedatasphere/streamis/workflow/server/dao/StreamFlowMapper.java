2d8efcd39967be3ebe69bba85eb153814294d1b2
yangzhiyue <904666286@qq.com>
2021-04-26 23:10:59 +0800

    为jobamanger添加一个protocol来进行一下rpc

---
+++ streamis-workflow/streamis-workflow-server/src/main/java/com/webank/wedatasphere/streamis/workflow/server/dao/StreamFlowMapper.java
@@ -0,0 +1,13 @@
+package com.webank.wedatasphere.streamis.workflow.server.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * created by yangzhiyue on 2021/4/26
+ * Description:
+ * 按照设计的思路应该是要在streamis这里也存一份streamis flow的相关信息，方便以后进行查看
+ *
+ */
+@Mapper
+public interface StreamFlowMapper {
+}
