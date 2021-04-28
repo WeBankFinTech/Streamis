7130e9ea84404e65240b55e39f5b366aaf7412b1
司海洋 <sihy@chinatelecom.cn>
2021-04-28 18:31:33 +0800

    remove mock api

---
+++ web/src/apps/streamis/common/common.js
@@ -0,0 +1,13 @@
+export const jobStatuses = [
+  { name: "failure", code: 6, icon: "md-close-circle", color: "#990033" },
+  { name: "running", code: 5, color: "#008000" },
+  { name: "slowTask", code: 4, icon: "md-help-circle", color: "#6666FF" },
+  { name: "alert", code: 3, icon: "md-warning", color: "#FF99CC" },
+  { name: "waitRestart", code: 2, icon: "md-alert", color: "#FF00CC" },
+  {
+    name: "success",
+    code: 1,
+    icon: "md-checkmark-circle",
+    color: "#008000"
+  }
+];
