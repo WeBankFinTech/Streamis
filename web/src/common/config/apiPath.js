9f4c43e586c35a0cc54afc582df7b2ee2c5dc9ee
yangzhiyue <904666286@qq.com>
2021-05-31 16:58:06 +0800

    修改下前端api路径 配合streamis的path

--- web/src/common/config/apiPath.js
+++ web/src/common/config/apiPath.js
@@ -2,6 +2,6 @@ export default {
   WORKSPACE_PATH: '/dss/framework/workspace/',
   PROJECT_PATH: '/dss/framework/project/',
   ORCHESTRATOR_PATH: '/dss/framework/orchestrator/',
-  WORKFLOW_PATH: '/dss/workflow/',
+  WORKFLOW_PATH: '/streamis/',
   PUBLISH_PATH: '/dss/framework/release/'
 }
