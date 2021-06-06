03a259bdf608430638f69c9ce19b4a4030829963
wushengyeyouya <690574002@qq.com>
2021-06-06 22:46:20 +0800

    Add the logical codes of launching StreamJob of Streamis to Linkis.

---
+++ streamis-jobmanager/streamis-job-manager/streamis-job-manager-service/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/manager/exception/JobExecuteFailedErrorException.scala
@@ -0,0 +1,13 @@
+package com.webank.wedatasphere.streamis.jobmanager.manager.exception
+
+import com.webank.wedatasphere.linkis.common.exception.ErrorException
+
+/**
+ *
+ * @date 2021-06-06
+ * @author enjoyyin
+ * @since 0.5.0
+ */
+class JobExecuteFailedErrorException(errorCode: Int, errorMsg: String) extends ErrorException(errorCode, errorMsg)
+
+class JobStopFailedErrorException(errorCode: Int, errorMsg: String) extends ErrorException(errorCode, errorMsg)
