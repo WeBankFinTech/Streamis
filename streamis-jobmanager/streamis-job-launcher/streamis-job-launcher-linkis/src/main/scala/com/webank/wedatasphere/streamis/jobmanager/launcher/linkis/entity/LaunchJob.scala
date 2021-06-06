3a1c05c5972e023c5c17899d5dfb19f91072382d
wushengyeyouya <690574002@qq.com>
2021-06-06 22:46:20 +0800

    Add the logical codes of launching StreamJob of Streamis to Linkis.

--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/entity/LaunchJob.scala
+++ streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/entity/LaunchJob.scala
@@ -1,7 +1,5 @@
 package com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.entity
 
-import com.webank.wedatasphere.streamis.jobmanager.launcher.entity.vo.ConfigKeyVO
-
 import java.util
 
 /**
@@ -21,5 +19,56 @@ trait LaunchJob {
   def getParams: util.Map[String, Any]
 
   def getSource: util.Map[String, Any]
+
+}
+
+object LaunchJob {
+
+  def builder(): Builder = new Builder
+
+  class Builder {
+    private var submitUser: String = _
+    private var labels: util.Map[String, Any] = _
+    private var jobContent: util.Map[String, Any] = _
+    private var params: util.Map[String, Any] = _
+    private var source: util.Map[String, Any] = _
+    def setSubmitUser(submitUser: String): this.type = {
+      this.submitUser = submitUser
+      this
+    }
+    def setLabels(labels: util.Map[String, Any]): this.type = {
+      this.labels = labels
+      this
+    }
+    def setJobContent(jobContent: util.Map[String, Any]): this.type = {
+      this.jobContent = jobContent
+      this
+    }
+    def setParams(param: util.Map[String, Any]): this.type = {
+      this.params = param
+      this
+    }
+    def setSource(source: util.Map[String, Any]): this.type = {
+      this.source = source
+      this
+    }
+    def setLaunchJob(launchJob: LaunchJob): this.type = {
+      setSubmitUser(launchJob.getSubmitUser).setLabels(launchJob.getLabels)
+        .setJobContent(launchJob.getJobContent).setParams(launchJob.getParams).setSource(launchJob.getSource)
+    }
+    def build(): LaunchJob = new LaunchJob {
+      override def getSubmitUser: String = submitUser
+
+      override def getLabels: util.Map[String, Any] = labels
+
+      override def getJobContent: util.Map[String, Any] = jobContent
+
+      override def getParams: util.Map[String, Any] = params
+
+      override def getSource: util.Map[String, Any] = source
+
+      override def toString: String = s"LaunchJob(submitUser: $submitUser, labels: $labels, jobContent: $jobContent, params: $params, source: $source)"
+    }
+  }
 
 }
