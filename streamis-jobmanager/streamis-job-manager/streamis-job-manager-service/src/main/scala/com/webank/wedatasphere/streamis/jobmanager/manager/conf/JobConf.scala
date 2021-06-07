c14e8fff836175e735a4fbcb4d9f49060c5424f0
wushengyeyouya <690574002@qq.com>
2021-06-07 12:31:47 +0800

    Add the logical codes of launching StreamJob of Streamis to Linkis.

--- streamis-jobmanager/streamis-job-manager/streamis-job-manager-service/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/manager/conf/JobConf.scala
+++ streamis-jobmanager/streamis-job-manager/streamis-job-manager-service/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/manager/conf/JobConf.scala
@@ -1,6 +1,6 @@
 package com.webank.wedatasphere.streamis.jobmanager.manager.conf
 
-import com.webank.wedatasphere.linkis.common.conf.CommonVars
+import com.webank.wedatasphere.linkis.common.conf.{CommonVars, TimeType}
 import com.webank.wedatasphere.streamis.jobmanager.manager.exception.JobExecuteFailedErrorException
 
 /**
@@ -8,19 +8,19 @@ import com.webank.wedatasphere.streamis.jobmanager.manager.exception.JobExecuteF
  */
 object JobConf {
 
-  val JOBMANAGER_FLINK_JOB_STATUS_ONE = CommonVars("wds.linkis.flink.job.status.one", 1,"complete")
+  val JOBMANAGER_FLINK_JOB_STATUS_ONE = CommonVars("wds.streamis.job.status.one", 1,"Completed")
 
-  val JOBMANAGER_FLINK_JOB_STATUS_TWO = CommonVars("wds.linkis.flink.job.status.two", 2,"Wait for restart")
+  val JOBMANAGER_FLINK_JOB_STATUS_TWO = CommonVars("wds.streamis.job.status.two", 2,"Wait for restart")
 
-  val JOBMANAGER_FLINK_JOB_STATUS_THREE = CommonVars("wds.linkis.flink.job.status.three", 3,"Alert")
+  val JOBMANAGER_FLINK_JOB_STATUS_THREE = CommonVars("wds.streamis.job.status.three", 3,"Alert running")
 
-  val JOBMANAGER_FLINK_JOB_STATUS_FOUR = CommonVars("wds.linkis.flink.job.status.four", 4,"Slow task")
+  val JOBMANAGER_FLINK_JOB_STATUS_FOUR = CommonVars("wds.streamis.job.status.four", 4,"Slow running")
 
-  val JOBMANAGER_FLINK_JOB_STATUS_FIVE = CommonVars("wds.linkis.flink.job.status.five", 5,"run")
+  val JOBMANAGER_FLINK_JOB_STATUS_FIVE = CommonVars("wds.streamis.job.status.five", 5,"running")
 
-  val JOBMANAGER_FLINK_JOB_STATUS_SIX = CommonVars("wds.linkis.flink.job.status.six", 6,"Failed task")
+  val JOBMANAGER_FLINK_JOB_STATUS_SIX = CommonVars("wds.streamis.job.status.six", 6,"Failed")
 
-  val JOBMANAGER_FLINK_JOB_STATUS_SEVEN = CommonVars("wds.linkis.flink.job.status.seven", 7,"Stop task")
+  val JOBMANAGER_FLINK_JOB_STATUS_SEVEN = CommonVars("wds.streamis.job.status.seven", 7,"Stopped")
 
   val STATUS_ARRAY = Array(JOBMANAGER_FLINK_JOB_STATUS_ONE, JOBMANAGER_FLINK_JOB_STATUS_TWO, JOBMANAGER_FLINK_JOB_STATUS_THREE,
     JOBMANAGER_FLINK_JOB_STATUS_FOUR, JOBMANAGER_FLINK_JOB_STATUS_FIVE, JOBMANAGER_FLINK_JOB_STATUS_SIX, JOBMANAGER_FLINK_JOB_STATUS_SEVEN)
@@ -33,9 +33,17 @@ object JobConf {
     case _ => false
   }
 
+  def linkisStatusToStreamisStatus(status: String): Int = status.toLowerCase match {
+    case "starting" | "unlock" | "locked" | "idle" | "busy" | "running" => JOBMANAGER_FLINK_JOB_STATUS_FIVE.getValue
+    case "success" => JOBMANAGER_FLINK_JOB_STATUS_ONE.getValue
+    case "failed" | "shuttingdown" => JOBMANAGER_FLINK_JOB_STATUS_SIX.getValue
+  }
+
   def getStatusString(status: Int): String = STATUS_ARRAY.find(_.getValue == status).map(_.description)
     .getOrElse(throw new JobExecuteFailedErrorException(30351, s"Unknown status $status."))
 
-  val JOBMANAGER_FLINK_SQL = CommonVars("wds.linkis.flink.job.sql", 1,"flink type sql")
+  val JOBMANAGER_FLINK_SQL = CommonVars("wds.streamis.job.type.sql", 1,"flink type sql")
+
+  val TASK_MONITOR_INTERVAL = CommonVars("wds.streamis.task.monitor.interval", new TimeType("1m"))
 
 }
