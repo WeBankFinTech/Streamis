da4054dcb48f54e53cbe7fe35c273b4a4968ba16
kongslove <42604208+kongslove@users.noreply.github.com>
2021-06-10 16:36:53 +0800

    add interface for get columns

--- streamis-datasource/streamis-datasource-manager/src/main/java/com/webank/wedatasphere/streamis/datasource/manager/domain/StreamisDataSourceTableVO.java
+++ streamis-datasource/streamis-datasource-manager/src/main/java/com/webank/wedatasphere/streamis/datasource/manager/domain/StreamisDataSourceTableVO.java
@@ -4,6 +4,7 @@ package com.webank.wedatasphere.streamis.datasource.manager.domain;
 public class StreamisDataSourceTableVO {
     private String tableName;
     private Boolean isStreamisDataSource;
+    private Long streamisTableMetaId;
 
     public String getTableName() {
         return tableName;
@@ -21,12 +22,20 @@ public class StreamisDataSourceTableVO {
         isStreamisDataSource = streamisDataSource;
     }
 
+    public Long getStreamisTableMetaId() {
+        return streamisTableMetaId;
+    }
+
+    public void setStreamisTableMetaId(Long streamisTableMetaId) {
+        this.streamisTableMetaId = streamisTableMetaId;
+    }
 
     @Override
     public String toString() {
         return "StreamisDataSourceTableVO{" +
                 "tableName='" + tableName + '\'' +
                 ", isStreamisDataSource=" + isStreamisDataSource +
+                ", streamisTableMetaId=" + streamisTableMetaId +
                 '}';
     }
 }
