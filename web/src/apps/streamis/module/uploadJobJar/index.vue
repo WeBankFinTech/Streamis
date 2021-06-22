bf20ad72c931f4859d64f1da5f3ffc3135023cec
司海洋 <sihy@chinatelecom.cn>
2021-06-22 17:01:37 +0800

    update upload jar

--- web/src/apps/streamis/module/uploadJobJar/index.vue
+++ web/src/apps/streamis/module/uploadJobJar/index.vue
@@ -118,7 +118,6 @@ export default {
             .catch(e => {
               console.log(e)
               this.loading = false;
-              this.$emit("jarUploadSuccess");
             })
         }
       })
