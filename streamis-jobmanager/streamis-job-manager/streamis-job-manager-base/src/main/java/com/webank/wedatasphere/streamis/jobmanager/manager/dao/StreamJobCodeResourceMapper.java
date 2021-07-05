76fe4564d76fbfa6f44cf507658a1d785d112af7
77954309@qq.com <Hkqo@9v738mUbvsP#>
2021-07-05 16:28:02 +0800

    bml

---
+++ streamis-jobmanager/streamis-job-manager/streamis-job-manager-base/src/main/java/com/webank/wedatasphere/streamis/jobmanager/manager/dao/StreamJobCodeResourceMapper.java
@@ -0,0 +1,15 @@
+package com.webank.wedatasphere.streamis.jobmanager.manager.dao;
+
+import com.webank.wedatasphere.streamis.jobmanager.manager.entity.StreamJobCodeResource;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface StreamJobCodeResourceMapper {
+
+    List<StreamJobCodeResource> getJobCodeList(@Param("jobVersionId") Long jobVersionId,@Param("bmlVersionId") Long bmlVersionId);
+
+    void insert(StreamJobCodeResource streamJobCodeResource);
+
+    void update(StreamJobCodeResource streamJobCodeResource);
+}
