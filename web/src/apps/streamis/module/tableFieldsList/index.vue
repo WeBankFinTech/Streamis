bf05c044eb5e336c89d9fd5fe90cf0f1ee71c8e4
ryanlei <ryanlei@webank.com>
2021-07-14 17:49:21 +0800

    数据源配置保存联调

--- web/src/apps/streamis/module/tableFieldsList/index.vue
+++ web/src/apps/streamis/module/tableFieldsList/index.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="table-list">
-    <Table :columns="columns" :data="tableDatas" :loading="loading">
+    <Table :columns="columns" :data="tableDatas">
       <template slot-scope="{ row, index }" slot="fieldName">
         <div class="fieldName" v-if="index === 0" @click="addColumn(row, index, tableDatas)">
-          <Icon type="md-add" class="addField"/>
+          <Icon type="md-add" class="addField" />
           <span>新增字段</span>
         </div>
         <div class="fieldName" v-else>
-          <Icon type="md-more" class="more"/>
-          <div v-if="!row.edit" style="margin-left: 5px">
-            {{ row.fieldName }}
-          </div>
+          <Icon type="md-more" class="more" />
+          <div v-if="!row.edit" style="margin-left: 5px">{{ row.fieldName }}</div>
           <Input v-else v-model="tableColumn.fieldName" placeholder="字段名称" style="width: 100px" />
         </div>
       </template>
@@ -26,7 +24,7 @@
       </template>
       <template slot-scope="{ row, index }" slot="fieldIsPartition">
         <div v-if="!row.edit && index!=0" style="margin-left: 5px">
-          <Radio label="是"  :value="true"  v-if="row.fieldIsPartition===1"></Radio>
+          <Radio label="是" :value="true" v-if="row.fieldIsPartition===1"></Radio>
           <span v-else>否</span>
         </div>
         <RadioGroup v-if="row.edit && index!=0" @on-change="funPartition">
@@ -35,28 +33,25 @@
         </RadioGroup>
       </template>
       <template slot-scope="{ row }" slot="fieldType">
-        <div v-if="!row.edit" style="margin-left: 5px">
-          {{ row.fieldType }}
-        </div>
+        <div v-if="!row.edit" style="margin-left: 5px">{{ row.fieldType }}</div>
         <Input v-else v-model="tableColumn.fieldType" placeholder="字段类型" style="width: 100px" />
       </template>
       <template slot-scope="{ row }" slot="verifyRule">
-        <div v-if="!row.edit" style="margin-left: 5px">
-          {{ row.verifyRule }}
-        </div>
+        <div v-if="!row.edit" style="margin-left: 5px">{{ row.verifyRule }}</div>
         <Input v-else v-model="tableColumn.verifyRule" placeholder="检测规则" style="width: 100px" />
       </template>
       <template slot-scope="{ row }" slot="fieldAlias">
-        <div v-if="!row.edit" style="margin-left: 5px">
-          {{ row.fieldAlias }}
-        </div>
+        <div v-if="!row.edit" style="margin-left: 5px">{{ row.fieldAlias }}</div>
         <Input v-else v-model="tableColumn.fieldAlias" placeholder="字段别名" style="width: 100px" />
       </template>
       <template slot-scope="{ row }" slot="fieldDescription">
-        <div v-if="!row.edit" style="margin-left: 5px">
-          {{ row.fieldDescription }}
-        </div>
-        <Input v-else v-model="tableColumn.fieldDescription" placeholder="字段说明" style="width: 100px" />
+        <div v-if="!row.edit" style="margin-left: 5px">{{ row.fieldDescription }}</div>
+        <Input
+          v-else
+          v-model="tableColumn.fieldDescription"
+          placeholder="字段说明"
+          style="width: 100px"
+        />
       </template>
       <template slot-scope="{ row, index }" slot="operation">
         <div v-if="!row.edit && index !== 0">
@@ -64,12 +59,9 @@
             type="primary"
             size="small"
             style="margin-right: 5px"
-            @click="editColumn(row, index, tableDatas)">
-            修改
-          </Button>
-          <Button type="error" size="small" @click="deleteColumn(row, index)">
-            删除
-          </Button>
+            @click="editColumn(row, index, tableDatas)"
+          >修改</Button>
+          <Button type="error" size="small" @click="deleteColumn(row, index)">删除</Button>
         </div>
         <!--第一列隐藏确定和取消 -->
         <div v-else-if="row.edit && index !== 0">
@@ -77,160 +69,148 @@
             type="primary"
             size="small"
             style="margin-right: 5px"
-            @click="submit(row, index)">
-            确定
-          </Button>
-          <Button type="error" size="small" @click="cancelColumn(row, index)">
-            取消
-          </Button>
+            @click="submit(row, index)"
+          >确定</Button>
+          <Button type="error" size="small" @click="cancelColumn(row, index)">取消</Button>
         </div>
       </template>
     </Table>
   </div>
 </template>
 <script>
-import api from "@/common/service/api";
 /**
  *
  */
 function renderSpecialHeader(h, params) {
-  return h("div", [
-    h("strong", params.column.title),
+  return h('div', [
+    h('strong', params.column.title),
     h(
-      "span",
+      'span',
       {
         style: {
-          color: "red"
+          color: 'red'
         }
       },
-      "*"
+      '*'
     )
-  ]);
+  ])
 }
 
 export default {
-  props: ["nodeId"],
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    tableMetaId: Number
+  },
   data() {
     return {
       deleteNotice: false,
       mapTableList: '',
-      //tableInfoFlag: '',
       //保留编辑前的初始值
       saveDatas: '',
+      tableDatas: this.data.slice(0),
       tableColumn: {},
       query: {
-        jobName: "",
-        jobStatus: "all",
-        submitter: "all"
+        jobName: '',
+        jobStatus: 'all',
+        submitter: 'all'
       },
-      jobStatus: ["all", "running", "failture", "uploaded"],
-      submitterOptions: ["all"],
-      tableDatas: [],
+      jobStatus: ['all', 'running', 'failture', 'uploaded'],
+      submitterOptions: ['all'],
       columns: [
         {
-          title: "字段名称",
-          key: "fieldName",
+          title: '字段名称',
+          key: 'fieldName',
           renderHeader: renderSpecialHeader,
-          slot: "fieldName"
+          slot: 'fieldName'
         },
         {
-          title: "是否主键",
-          key: "fieldIsPrimary",
+          title: '是否主键',
+          key: 'fieldIsPrimary',
           renderHeader: renderSpecialHeader,
-          slot: "fieldIsPrimary",
+          slot: 'fieldIsPrimary'
         },
         {
-          title: "分区字段",
-          key: "fieldIsPartition",
+          title: '分区字段',
+          key: 'fieldIsPartition',
           renderHeader: renderSpecialHeader,
-          slot: "fieldIsPartition"
+          slot: 'fieldIsPartition'
         },
         {
-          title: "字段类型",
-          key: "fieldType",
+          title: '字段类型',
+          key: 'fieldType',
           renderHeader: renderSpecialHeader,
-          slot: "fieldType"
+          slot: 'fieldType'
         },
         {
-          title: "检测规则",
-          key: "verifyRule",
-          slot: "verifyRule"
+          title: '检测规则',
+          key: 'verifyRule',
+          slot: 'verifyRule'
         },
         {
-          title: "字段别名",
-          key: "fieldAlias",
-          slot: "fieldAlias"
+          title: '字段别名',
+          key: 'fieldAlias',
+          slot: 'fieldAlias'
         },
         {
-          title: "字段说明",
-          key: "fieldDescription",
-          slot: "fieldDescription"
+          title: '字段说明',
+          key: 'fieldDescription',
+          slot: 'fieldDescription'
         },
         {
-          title: this.$t("message.streamis.jobListTableColumns.operation"),
-          key: "operation",
-          slot: "operation"
+          title: this.$t('message.streamis.jobListTableColumns.operation'),
+          key: 'operation',
+          slot: 'operation'
         }
       ],
       jobMoudleRouter: [
-        "paramsConfiguration",
-        "alertConfiguration",
-        "runningHistory",
-        "runningLogs"
-      ],
-      loading: false,
-      changeFieldList: false
-    };
+        'paramsConfiguration',
+        'alertConfiguration',
+        'runningHistory',
+        'runningLogs'
+      ]
+    }
   },
-  // 监听从父组件传过来的二级菜单的Id
   watch: {
-    nodeId(){
-      this.getFieldsList();
-    },
-    tableDatas: {
-      handler() {
-        this.$emit('funTableColumn',this.tableDatas)
-      },
-      deep: true,
-    },
+    data(v) {
+      this.tableDatas = v.slice(0)
+    }
   },
-  mounted() {
-    this.getFieldsList();
-  },
+  mounted() {},
   methods: {
-    funPrimary(params){
-      this.tableColumn.fieldIsPrimary = (params=='是'?1:0)
+    funPrimary(params) {
+      this.tableColumn.fieldIsPrimary = params == '是' ? 1 : 0
     },
-    funPartition(params){
-      this.tableColumn.fieldIsPartition = (params=='是'?1:0)
+    funPartition(params) {
+      this.tableColumn.fieldIsPartition = params == '是' ? 1 : 0
     },
     //删除字段
-    deleteColumn(row){
-      let index = this.tableDatas.findIndex(subitem => subitem.id === row.id )
+    deleteColumn(row) {
+      let index = this.tableDatas.findIndex(subitem => subitem.id === row.id)
       this.tableDatas.splice(index, 1)
-      // 直接把tableDatas传递过去
-      this.$emit('funTableColumn',this.tableDatas)
-      this.changeFieldList = true
-      this.$emit('funChangeFieldList',this.changeFieldList)
+      this.$emit('change', true, this.tableDatas)
     },
-    submit(row, index){
-      // 如果没有任何改变不能点击确定按钮 禁用确定按钮
-      // 只要点击过确定就是有改变
-      this.changeFieldList = true
-      this.$emit('funChangeFieldList',this.changeFieldList)
+    submit(row, index) {
       const objectKey = {
-        fieldName: "字段名称",
-        fieldIsPrimary: "是否是主键",
-        fieldIsPartition: "是否是分区",
-        fieldType: "是否是字段类型"
+        fieldName: '字段名称',
+        fieldIsPrimary: '是否是主键',
+        fieldIsPartition: '是否是分区',
+        fieldType: '是否是字段类型'
       }
       const keys = Object.keys(this.tableColumn)
       const findItem = keys.find(item => this.tableColumn[item] === '')
-      if(findItem && findItem !== 'fieldDescription' && findItem !== 'fieldAlias' && findItem !== 'verifyRule'){
+      if (
+        findItem &&
+        findItem !== 'fieldDescription' &&
+        findItem !== 'fieldAlias' &&
+        findItem !== 'verifyRule'
+      ) {
         this.$Message.error(`请输入${objectKey[findItem]}`)
         return
       }
-      this.tableDatas.splice(index, 1,{
+      this.tableDatas.splice(index, 1, {
         fieldName: this.tableColumn.fieldName,
         fieldType: this.tableColumn.fieldType,
         fieldIsPrimary: this.tableColumn.fieldIsPrimary,
@@ -238,15 +218,15 @@ export default {
         verifyRule: this.tableColumn.verifyRule,
         fieldAlias: this.tableColumn.fieldAlias,
         fieldDescription: this.tableColumn.fieldDescription,
-        streamisTableMetaId: this.nodeId
+        streamisTableMetaId: this.tableMetaId
       })
       // 直接把tableDatas传递过去
-      this.$emit('funTableColumn',this.tableDatas)
+      this.$emit('change', true, this.tableDatas)
     },
     //取消新增字段
-    cancelColumn(row, index){
+    cancelColumn(row, index) {
       //如果是添加的取消按钮 我们就直接取消一行
-      if(!this.saveDatas){
+      if (!this.saveDatas) {
         this.tableDatas.splice(index, 1)
       } else {
         //点击取消编辑框里面的数据恢复到原始值
@@ -258,12 +238,12 @@ export default {
           verifyRule: this.saveDatas[4],
           fieldAlias: this.saveDatas[5],
           fieldDescription: this.saveDatas[6],
-          edit: false,
+          edit: false
         })
       }
     },
     //新增字段
-    addColumn(row, index){
+    addColumn(row, index) {
       this.tableColumn.fieldName = ''
       this.tableColumn.fieldIsPrimary = ''
       this.tableColumn.fieldIsPartition = ''
@@ -274,35 +254,9 @@ export default {
       this.tableDatas.splice(index + 1, 0, { edit: true })
       this.saveDatas = ''
     },
-    getFieldsList() {
-      //有星星的时候删除加的第一行空对象
-      //this.tableDatas.splice(0,1)
-      let params = this.nodeId
-      if(this.nodeId){
-        api
-          .fetch("streamis/streamisTableMetaInfo/" + params , "get")
-          .then(res => {
-            if (res) {
-              const datas = res.streamisDatasourceFields || [];
-              datas.unshift({});
-              this.tableDatas = datas;
-              //拿到表信息的字段 传给父组件 然后父组件再传递给子组件 子组件拿到信息直接渲染出来
-              let formData = res.streamisTableMeta
-              this.$emit('tableInfoFun',formData)
-              // 存储streamisExtraInfo 传递给父组件翻译用
-              let streamisExtraInfo = res.streamisExtraInfo || []
-              this.$emit('extraInfoFun',streamisExtraInfo)
-            }
-          }).catch(e => console.log(e));
-      }else{
-        // 如果没有id就不要发送请求
-        this.tableDatas = []
-        this.tableDatas.unshift({})
-      }
-    },
     //修改字段
     editColumn(row, index) {
-      this.tableDatas = this.tableDatas.filter((item) => !item.edit)
+      this.tableDatas = this.tableDatas.filter(item => !item.edit)
       this.tableDatas.splice(index, 1, { edit: true })
       this.tableColumn.fieldName = row.fieldName
       this.tableColumn.fieldIsPrimary = row.fieldIsPrimary
@@ -312,32 +266,39 @@ export default {
       this.tableColumn.fieldAlias = row.fieldAlias
       this.tableColumn.fieldDescription = row.fieldDescription
       //保存修改后的数据
-      this.saveDatas = [row.fieldName, row.fieldIsPrimary, row.fieldIsPartition, row.fieldType, row.verifyRule, row.fieldAlias, row.fieldDescription]
-      console.log(row.fieldIsPrimary,row.fieldIsPrimary,"保存的修改后的值")
-    },
+      this.saveDatas = [
+        row.fieldName,
+        row.fieldIsPrimary,
+        row.fieldIsPartition,
+        row.fieldType,
+        row.verifyRule,
+        row.fieldAlias,
+        row.fieldDescription
+      ]
+    }
   }
-};
+}
 </script>
 <style lang="scss" scoped>
-.table-list{
-padding: 10px 30px;
-.select {
-  width: 200px;
-}
-.fieldName {
-  display: flex;
-  cursor: pointer;
-}
-.more {
-  font-size: 20px;
-}
-.addField {
-  font-size: 24px;
-  font-weight: 700;
-  margin-right: 5px;
-}
-.page {
-  margin-top: 20px;
-}
+.table-list {
+  padding: 10px 30px;
+  .select {
+    width: 200px;
+  }
+  .fieldName {
+    display: flex;
+    cursor: pointer;
+  }
+  .more {
+    font-size: 20px;
+  }
+  .addField {
+    font-size: 24px;
+    font-weight: 700;
+    margin-right: 5px;
+  }
+  .page {
+    margin-top: 20px;
+  }
 }
 </style>
