dcc26c855298ddb7570b70768b4cb979612f9a2d
wushengyeyouya <690574002@qq.com>
2021-11-05 00:18:01 +0800

    Initial Commits for Streamis server.

--- streamis-jobmanager/streamis-jobmanager-server/src/main/java/com/webank/wedatasphere/streamis/jobmanager/restful/api/ProjectRestfulApi.java
+++ streamis-jobmanager/streamis-jobmanager-server/src/main/java/com/webank/wedatasphere/streamis/jobmanager/restful/api/ProjectRestfulApi.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2021 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.webank.wedatasphere.streamis.jobmanager.restful.api;
 
 import com.webank.wedatasphere.linkis.server.Message;
-import com.webank.wedatasphere.linkis.server.security.SecurityFilter;
 import com.webank.wedatasphere.streamis.jobmanager.exception.ProjectException;
 import com.webank.wedatasphere.streamis.jobmanager.manager.entity.vo.TaskCoreNumVO;
 import com.webank.wedatasphere.streamis.jobmanager.manager.service.JobService;
-import com.webank.wedatasphere.streamis.jobmanager.manager.service.ProjectService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,18 +35,17 @@ import java.io.IOException;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProjectRestfulApi {
-    @Autowired
-    ProjectService projectService;
+
     @Autowired
     JobService jobService;
     @GET
     @Path("/core/target")
-    public Response getView(@Context HttpServletRequest req, @QueryParam("projectId") Long projectId) throws IOException, ProjectException {
+    public Response getView(@Context HttpServletRequest req, @QueryParam("projectName") String projectName) throws IOException, ProjectException {
 
-        if(projectId==null){
+        if(StringUtils.isBlank(projectName)){
             throw new ProjectException("params cannot be empty!");
         }
-        TaskCoreNumVO taskCoreNumVO = jobService.countByCores(projectId);
+        TaskCoreNumVO taskCoreNumVO = jobService.countByCores(projectName);
         return  Message.messageToResponse(Message.ok().data("taskCore",taskCoreNumVO));
     }
 }
