da0cb0a8aefa1b8ea6d7572fb77446e6db66cfd2
liangx <46127018+15100399015@users.noreply.github.com>
2021-11-05 19:57:01 +0800

    fix routepath / to streamis

--- web/src/apps/streamis/router.js
+++ web/src/apps/streamis/router.js
@@ -1,5 +1,15 @@
 export default [
   {
+    path: '/',
+    meta: {
+      title: 'Streamis',
+      keepAlive: false, // 缓存导致页面有多个编辑器，广播事件会触发报错
+      publicPage: true, // 权限公开
+    },
+    component: () =>
+      import('./view/realTimeJobCenter/index.vue'),
+  },
+  {
     path: '/realTimeJobCenter',
     name: 'RealTimeJobCenter',
     meta: {
