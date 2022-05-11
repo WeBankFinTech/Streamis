5b08d5fa9ba0e781886492c58c7932f7ef188f37
v_xiangbiaowu <v_xiangbiaowu@webank.com>
2022-05-11 18:26:45 +0800

    add streamis Appconn

--- streamis-appconn/src/main/java/com/webank/wedatasphere/streamis/dss/appconn/StreamisAppConn.java
+++ streamis-appconn/src/main/java/com/webank/wedatasphere/streamis/dss/appconn/StreamisAppConn.java
@@ -4,22 +4,27 @@ import com.webank.wedatasphere.dss.appconn.core.ext.ThirdlyAppConn;
 import com.webank.wedatasphere.dss.appconn.core.impl.AbstractOnlySSOAppConn;
 import com.webank.wedatasphere.dss.standard.app.development.standard.DevelopmentIntegrationStandard;
 import com.webank.wedatasphere.dss.standard.app.structure.StructureIntegrationStandard;
+import com.webank.wedatasphere.streamis.dss.appconn.structure.StreamisStructureIntegrationStandard;
+import org.apache.linkis.common.conf.CommonVars;
 
 public class StreamisAppConn extends AbstractOnlySSOAppConn implements ThirdlyAppConn {
+
+    public static final String STREAMIS_APPCONN_NAME = CommonVars.apply("wds.dss.appconn.streamis.name", "Streamis").getValue();
+
+    private StreamisStructureIntegrationStandard structureIntegrationStandard;
+
     @Override
     public DevelopmentIntegrationStandard getOrCreateDevelopmentStandard() {
-        // Not to use the development flow, so return null
         return null;
     }
 
     @Override
     public StructureIntegrationStandard getOrCreateStructureStandard() {
-
-        return null;
+        return structureIntegrationStandard;
     }
 
     @Override
     protected void initialize() {
-
+        structureIntegrationStandard = new StreamisStructureIntegrationStandard();
     }
 }
