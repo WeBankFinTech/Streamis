a41f590a2885dc37ec6fe99e1977cfa3519df94a
davidhua <david_hua1996@hotmail.com>
2022-05-13 15:44:00 +0800

    Refresh the TODO list

--- streamis-appconn/src/main/java/com/webank/wedatasphere/streamis/dss/appconn/structure/project/StreamisProjectService.java
+++ streamis-appconn/src/main/java/com/webank/wedatasphere/streamis/dss/appconn/structure/project/StreamisProjectService.java
@@ -8,23 +8,27 @@ import com.webank.wedatasphere.streamis.dss.appconn.structure.ref.StreamisProjec
  */
 public class StreamisProjectService extends ProjectService {
 
+    // TODO use the StreamisProjectContentReqRef as parameter type
     @Override
     protected ProjectCreationOperation createProjectCreationOperation() {
         return new StreamisProjectCreationOperation();
     }
 
+    // TODO use the StreamisProjectUpdateReqRef as parameter type
     @Override
     protected ProjectUpdateOperation createProjectUpdateOperation() {
         return new StreamisProjectUpdateOperation();
     }
 
 
+    // TODO deletion operation
     @Override
     protected ProjectDeletionOperation<?> createProjectDeletionOperation() {
         // Not need to delete the project
         return null;
     }
 
+    // TODO query operation
     @Override
     protected ProjectSearchOperation<StreamisProjectContentReqRef> createProjectSearchOperation() {
         //TODO create search operation
