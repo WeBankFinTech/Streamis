7fbf73338ef9490e66596d748a516f56fa80fac2
David hua <david_hua1996@hotmail.com>
2022-06-22 18:17:38 +0800

    Revert "[Feature] JobState component in launcher module"

--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/java/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/state/AbstractLinkisJobStateFetcher.java
+++
@@ -1,4 +0,0 @@
-package com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.state;
-
-public class AbstractLinkisJobStateFetcher {
-}
--- streamis-project/streamis-project-server/src/main/java/com/webank/wedatasphere/streamis/project/server/entity/request/DeleteProjectRequest.java
+++ streamis-project/streamis-project-server/src/main/java/com/webank/wedatasphere/streamis/project/server/entity/request/DeleteProjectRequest.java
@@ -7,21 +7,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class DeleteProjectRequest {
 
-    private Long projectId;
-
     /**
      * streamis工程是全局唯一的
      */
     @NotNull(message = "streamis projetName can not null")
     private String projectName;
 
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
 
     public String getProjectName() {
         return projectName;
