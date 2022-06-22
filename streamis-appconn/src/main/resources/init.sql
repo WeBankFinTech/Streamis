998e0afcd80e69543e636a91155fdc250bdd1083
David hua <david_hua1996@hotmail.com>
2022-06-22 18:17:38 +0800

    Revert "[Feature] JobState component in launcher module"

--- streamis-appconn/src/main/resources/init.sql
+++
@@ -1,4 +0,0 @@
-
-delete from  `dss_appconn` where `appconn_name`='streamis';
-INSERT INTO `dss_appconn` (`appconn_name`, `is_user_need_init`, `level`, `if_iframe`, `is_external`, `reference`, `class_name`, `appconn_class_path`, `resource`)
-VALUES ('streamis', 0, 1, NULL, 0, NULL, 'com.webank.wedatasphere.dss.appconn.streamis.StreamisAppConn', 'DSS_INSTALL_HOME_VAL/dss-appconns/streamis/lib', '');
