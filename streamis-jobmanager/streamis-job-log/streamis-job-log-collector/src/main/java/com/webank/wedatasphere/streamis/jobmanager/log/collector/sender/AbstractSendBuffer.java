4c4408988f894d793decc3e472972530fa8be161
davidhua <david_hua1996@hotmail.com>
2022-10-19 02:42:57 +0800

    SendBuffer and BucketConfig.

---
+++ streamis-jobmanager/streamis-job-log/streamis-job-log-collector/src/main/java/com/webank/wedatasphere/streamis/jobmanager/log/collector/sender/AbstractSendBuffer.java
@@ -0,0 +1,107 @@
+package com.webank.wedatasphere.streamis.jobmanager.log.collector.sender;
+
+/**
+ * Abstract sender buffer;
+ * non-blocking and reduces out-of-bounds exceptions
+ */
+public abstract class AbstractSendBuffer<E> implements SendBuffer<E>{
+
+    protected enum Flag{
+        WRITE_MODE, READ_MODE
+    }
+
+    /**
+     * Access flag
+     */
+    private Flag accessFlag = Flag.WRITE_MODE;
+
+    private int position = 0;
+    private int limit;
+    /**
+     * The capacity is mutable
+     */
+    protected int capacity;
+
+
+    public AbstractSendBuffer(int capacity){
+        this.capacity = capacity;
+        limit(this.capacity);
+    }
+
+    public AbstractSendBuffer(){
+        this(Integer.MAX_VALUE);
+    }
+    @Override
+    public int capacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public int remaining() {
+        int rem = this.limit - this.position;
+        return Math.max(rem, 0);
+    }
+
+    @Override
+    public void flip() {
+        this.limit = this.position;
+        this.position = 0;
+        this.accessFlag = Flag.READ_MODE;
+    }
+
+    @Override
+    public void clear() {
+        limit(this.capacity);
+        this.position = 0;
+        this.accessFlag = Flag.WRITE_MODE;
+        clearBuf();
+    }
+
+    /**
+     * Change the limit value
+     * @param newLimit new limit
+     */
+    final void limit(int newLimit){
+        if (newLimit > this.capacity || (newLimit < 0)){
+            throw new IllegalArgumentException("Set the illegal limit value: " + newLimit + " in send buffer, [capacity: " + this.capacity + "]");
+        }
+        this.limit = newLimit;
+        if (this.position > newLimit){
+            this.position = newLimit;
+        }
+    }
+
+    /**
+     * Inc the position with offset
+     * @param offset offset value
+     * @param accessFlag access flag
+     * @return the current position value
+     */
+    final int nextPosition(int offset, Flag accessFlag){
+        if (this.accessFlag != accessFlag){
+            throw new IllegalStateException("Illegal access flag [" + accessFlag + "] for send buffer");
+        }
+        int p = position;
+        // Reach the limit, return -1 value
+        if (p >= limit){
+            return -1;
+        }
+        if (p + offset > limit){
+            this.position = limit;
+        }
+        return p;
+    }
+
+    /**
+     *
+     * @return the current position
+     */
+    final int position(){
+        return this.position;
+    }
+
+    /**
+     * Do the actual clear
+     */
+    protected abstract void clearBuf();
+}
--- streamis-jobmanager/streamis-job-log/streamis-job-log-server/src/main/java/com/webank/wedatasphere/streamis/jobmanager/log/server/storage/bucket/JobLogBucketConfig.java
+++ streamis-jobmanager/streamis-job-log/streamis-job-log-server/src/main/java/com/webank/wedatasphere/streamis/jobmanager/log/server/storage/bucket/JobLogBucketConfig.java
@@ -1,5 +1,8 @@
 package com.webank.wedatasphere.streamis.jobmanager.log.server.storage.bucket;
 
+import com.webank.wedatasphere.streamis.jobmanager.log.server.exception.StreamJobLogException;
+import org.apache.linkis.common.conf.CommonVars;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,10 +11,27 @@ import java.util.Map;
  */
 public class JobLogBucketConfig {
 
+    @SuppressWarnings("unchecked")
+    public JobLogBucketConfig(){
+        try {
+            Class<?> defaultBucketClass = Class.forName(Define.JOB_LOG_BUCKET_CLASS.getValue());
+            if (JobLogBucket.class.isAssignableFrom(defaultBucketClass)){
+                this.bucketClass = (Class<? extends JobLogBucket>) defaultBucketClass;
+            }
+        } catch (ClassNotFoundException e) {
+//            throw new StreamJobLogException.Runtime(-1, "", e);
+        }
+    }
+
     /**
      * Bucket class
      */
     private Class<? extends JobLogBucket> bucketClass;
+
+    /**
+     * Root path for bucket
+     */
+    private String bucketRootPath;
 
     /**
      * Attribute
@@ -19,14 +39,45 @@ public class JobLogBucketConfig {
     protected Map<String, Object> attributes = new HashMap<>();
 
     /**
-     * Max size of bucket part
+     * Max size of bucket active part
      */
-    private int maxBucketPartSize;
+    private int maxBucketActivePartSize;
+
+    /**
+     * Max number of bucket part
+     */
+    private int maxBucketPartNum;
+
+    /**
+     * The compress format used for bucket parts
+     */
+    private String bucketPartCompress;
+
+    /**
+     * Max hold time in minutes for bucket part
+     */
+    private long bucketPartHoldTimeInMin;
 
     /**
      * Layout pattern
      */
-    private String LayOutPattern;
+    private String LogLayOutPattern = "%msg%n";
+
+    public Class<? extends JobLogBucket> getBucketClass() {
+        return bucketClass;
+    }
+
+    public void setBucketClass(Class<? extends JobLogBucket> bucketClass) {
+        this.bucketClass = bucketClass;
+    }
+
+    public String getBucketRootPath() {
+        return bucketRootPath;
+    }
+
+    public void setBucketRootPath(String bucketRootPath) {
+        this.bucketRootPath = bucketRootPath;
+    }
 
     public Map<String, Object> getAttributes() {
         return attributes;
@@ -36,23 +87,51 @@ public class JobLogBucketConfig {
         this.attributes = attributes;
     }
 
-    public int getMaxBucketPartSize() {
-        return maxBucketPartSize;
+    public int getMaxBucketActivePartSize() {
+        return maxBucketActivePartSize;
     }
 
-    public void setMaxBucketPartSize(int maxBucketPartSize) {
-        this.maxBucketPartSize = maxBucketPartSize;
+    public void setMaxBucketActivePartSize(int maxBucketActivePartSize) {
+        this.maxBucketActivePartSize = maxBucketActivePartSize;
     }
 
-    public String getLayOutPattern() {
-        return LayOutPattern;
+    public int getMaxBucketPartNum() {
+        return maxBucketPartNum;
     }
 
-    public void setLayOutPattern(String layOutPattern) {
-        LayOutPattern = layOutPattern;
+    public void setMaxBucketPartNum(int maxBucketPartNum) {
+        this.maxBucketPartNum = maxBucketPartNum;
     }
+
+    public String getBucketPartCompress() {
+        return bucketPartCompress;
+    }
+
+    public void setBucketPartCompress(String bucketPartCompress) {
+        this.bucketPartCompress = bucketPartCompress;
+    }
+
+    public long getBucketPartHoldTimeInMin() {
+        return bucketPartHoldTimeInMin;
+    }
+
+    public void setBucketPartHoldTimeInMin(long bucketPartHoldTimeInMin) {
+        this.bucketPartHoldTimeInMin = bucketPartHoldTimeInMin;
+    }
+
+    public String getLogLayOutPattern() {
+        return LogLayOutPattern;
+    }
+
+    public void setLogLayOutPattern(String logLayOutPattern) {
+        LogLayOutPattern = logLayOutPattern;
+    }
+
 
     public static final class Define{
-
+        /**
+         * Default bucket class
+         */
+        public static final CommonVars<String> JOB_LOG_BUCKET_CLASS = CommonVars.apply("wds.streamis.job.log.bucket.class", "com.webank.wedatasphere.streamis.jobmanager.log.server.storage.bucket.Log4j2JobLogBucket");
     }
 }
