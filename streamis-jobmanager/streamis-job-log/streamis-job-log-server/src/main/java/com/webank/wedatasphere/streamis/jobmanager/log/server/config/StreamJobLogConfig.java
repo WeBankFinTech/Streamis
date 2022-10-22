6c680e37945709d6e491ec22791fe2c666d94a62
davidhua <david_hua1996@hotmail.com>
2022-10-23 02:30:01 +0800

    Complete stream log server to store the log events.

---
+++ streamis-jobmanager/streamis-job-log/streamis-job-log-server/src/main/java/com/webank/wedatasphere/streamis/jobmanager/log/server/config/StreamJobLogConfig.java
@@ -0,0 +1,46 @@
+package com.webank.wedatasphere.streamis.jobmanager.log.server.config;
+
+import org.apache.linkis.common.conf.CommonVars;
+import org.apache.linkis.common.conf.TimeType;
+
+/**
+ * Store the configuration defined for job log
+ */
+public class StreamJobLogConfig {
+
+    /**
+     * Bucket monitor name
+     */
+    public static final CommonVars<String> BUCKET_MONITOR_NAME = CommonVars.apply("wds.stream.job.log.storage.bucket.monitor.name", "Log-Storage-Bucket-Monitor");
+
+    /**
+     * Bucket monitor interval
+     */
+    public static final CommonVars<TimeType> BUCKET_MONITOR_INTERVAL = CommonVars.apply("wds.stream.job.log.storage.bucket.monitor.interval", new TimeType("2m"));
+
+    /**
+     * Bucket max idle time
+     */
+    public static final CommonVars<TimeType> BUCKET_MAX_IDLE_TIME = CommonVars.apply("wds.stream.job.log.storage.bucket.max-idle-time", new TimeType("12h"));
+
+    /**
+     * Bucket root path
+     */
+    public static final CommonVars<String> BUCKET_ROOT_PATH = CommonVars.apply("wds.stream.job.log.storage.bucket.root-path", "/data/stream/log");
+    /**
+     * Max active part size in bucket
+     */
+    public static final CommonVars<Long> BUCKET_MAX_ACTIVE_PART_SIZE = CommonVars.apply("wds.stream.job.log.storage.bucket.max-active-part-size", 100L);
+
+    /**
+     * Compression of part in bucket
+     */
+    public static final CommonVars<String> BUCKET_PART_COMPRESS = CommonVars.apply("wds.stream.job.log.storage.bucket.part-compress", "gz");
+
+    /**
+     * Bucket layout
+     */
+    public static final CommonVars<String> BUCKET_LAYOUT = CommonVars.apply("wds.stream.job.log.storage.bucket.layout", "%msg");
+
+    public static final CommonVars<Integer> BUCKET_PART_HOLD_DAY = CommonVars.apply("wds.stream.job.log.storage.bucket.part-hold-day", 30);
+}
