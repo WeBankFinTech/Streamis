85b98129c239597566ec4b4e1c0aa40ef3936ded
davidhua <david_hua1996@hotmail.com>
2022-10-27 02:48:23 +0800

    Complete the strategy in JsonTool.escapeStrValue().

--- streamis-jobmanager/streamis-job-log/streamis-job-log-common/src/main/java/com/webank/wedatasphere/streamis/jobmanager/log/json/JsonTool.java
+++ streamis-jobmanager/streamis-job-log/streamis-job-log-common/src/main/java/com/webank/wedatasphere/streamis/jobmanager/log/json/JsonTool.java
@@ -1,13 +1,16 @@
 package com.webank.wedatasphere.streamis.jobmanager.log.json;
 
+import java.util.Locale;
+
 public class JsonTool {
+    static final char[] HEX_DIGITS = new char[] {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
     /**
      * Avoid the special char
      * @param input input string
      * @return output string
      */
-    public static String encodeStrValue(String input){
+    public static String escapeStrValue(String input){
         char[] chars = input.toCharArray();
         StringBuilder sb = new StringBuilder();
         for (char c : chars) {
@@ -37,9 +40,24 @@ public class JsonTool {
                     sb.append("\\t");
                     break;
                 default:
-                    sb.append(c);
+                    sb.append((c < 32) ? escapeUnicode(c) : c);
             }
         }
         return sb.toString();
     }
+
+    /**
+     * Escape unicode
+     * @param code char code
+     * @return escaped string
+     */
+    private static String escapeUnicode(int code){
+        if (code > 0xffff){
+            return "\\u" + Integer.toHexString(code).toUpperCase(Locale.ENGLISH);
+        }  else {
+            return "\\u" + HEX_DIGITS[(code >> 12) & 15]
+                    + HEX_DIGITS[(code >> 8) & 15] + HEX_DIGITS[(code >> 4) & 15] + HEX_DIGITS[code & 15];
+        }
+    }
+
 }
