6683788a10ca907fa08be8bdf2023d46c5e08095
davidhua <david_hua1996@hotmail.com>
2022-12-25 19:55:55 +0800

    Inspections of stream job; Support to define job configuration in meta.json file.

---
+++ streamis-jobmanager/streamis-job-manager/streamis-job-manager-base/src/main/java/com/webank/wedatasphere/streamis/jobmanager/manager/entity/vo/JobInspectVo.java
@@ -0,0 +1,19 @@
+package com.webank.wedatasphere.streamis.jobmanager.manager.entity.vo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * Job inspect vo
+ */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public interface JobInspectVo {
+
+    enum Types{
+        VERSION, SNAPSHOT, STATUS
+    }
+    @JsonIgnore
+    String getInspectName();
+
+
+}
