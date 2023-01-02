d6ba0334f68d59cb1d28a666f355d8c224588f28
jefftlin <jefft_lin@163.com>
2023-01-03 01:17:03 +0800

    Job heartbeat module

---
+++ streamis-jobmanager/streamis-job-entrypoint/streamis-job-entrypoint-common/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/entrypoint/sender/SparkJavaStreamJobHeartbeatSender.scala
@@ -0,0 +1,17 @@
+package com.webank.wedatasphere.streamis.jobmanager.entrypoint.sender
+
+import com.webank.wedatasphere.streamis.jobmanager.entrypoint.config.SenderHttpConfig
+import org.apache.http.impl.client.CloseableHttpClient
+
+class SparkJavaStreamJobHeartbeatSender extends StreamJobHeartbeatSender {
+
+  /**
+   * Encapsulate the httpclient
+   *
+   * @return
+   */
+  override def send(httpClient: CloseableHttpClient, senderHttpConfig: SenderHttpConfig): Unit = {
+
+
+  }
+}
