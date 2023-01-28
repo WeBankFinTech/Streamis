ec93894e9712b36cc5c06a53640a6054a0e0b7d8
jefftlin <jefft_lin@163.com>
2023-01-29 01:29:06 +0800

    Update stream job heartbeat

--- streamis-jobmanager/streamis-job-entrypoint/streamis-job-entrypoint-common/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/entrypoint/producer/FlinkStreamJobHeartbeatProducer.scala
+++ streamis-jobmanager/streamis-job-entrypoint/streamis-job-entrypoint-common/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/entrypoint/producer/FlinkStreamJobHeartbeatProducer.scala
@@ -1,9 +1,24 @@
 package com.webank.wedatasphere.streamis.jobmanager.entrypoint.producer
+import com.webank.wedatasphere.streamis.jobmanager.entrypoint.config.{FLinkStreamJobProducerConfig, StreamJobProducerConfig}
+
+import java.util
 
 class FlinkStreamJobHeartbeatProducer extends StreamJobHeartbeatProducer {
 
-  override def produce(): Unit = {
+  override def produce(configMap: java.util.Map[String, Object]): Unit = {
+    val configuration = configMap.get("configuration")
+    val streamExecutionEnvironment = configMap.get("streamExecutionEnvironment")
+
+    val flinkConfig = getProducerConfig("flink", configMap)
 
 
+  }
+
+  override def getProducerConfig(engineType: String, configMap: util.Map[String, Object]): StreamJobProducerConfig = {
+    var producerConfig = new FLinkStreamJobProducerConfig
+    producerConfig.setApplicationUrl(configMap.get("applicationUrl").toString)
+    producerConfig.setJobId(configMap.get("jobId").toString)
+
+    producerConfig.asInstanceOf[StreamJobProducerConfig]
   }
 }
