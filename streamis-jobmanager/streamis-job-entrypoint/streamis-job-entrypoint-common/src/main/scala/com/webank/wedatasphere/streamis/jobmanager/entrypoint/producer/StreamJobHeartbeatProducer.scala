09724e9ca8fc8124ff55a560bc5bef79cbdf236d
jefftlin <jefft_lin@163.com>
2023-01-31 14:26:13 +0800

    Update stream job heartbeat entrypoint

--- streamis-jobmanager/streamis-job-entrypoint/streamis-job-entrypoint-common/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/entrypoint/producer/StreamJobHeartbeatProducer.scala
+++ streamis-jobmanager/streamis-job-entrypoint/streamis-job-entrypoint-common/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/entrypoint/producer/StreamJobHeartbeatProducer.scala
@@ -1,10 +1,11 @@
 package com.webank.wedatasphere.streamis.jobmanager.entrypoint.producer
 
-import com.webank.wedatasphere.streamis.jobmanager.entrypoint.config.StreamJobProducerConfig
+import com.webank.wedatasphere.streamis.jobmanager.entrypoint.config.{StreamJobConfig, StreamJobProducerConfig}
+import com.webank.wedatasphere.streamis.jobmanager.entrypoint.message.JobHeartbeatMessage
 
 trait StreamJobHeartbeatProducer {
 
-  def produce(configMap: java.util.Map[String, Object]): Unit
+  def produce(streamJobConfig: StreamJobConfig): JobHeartbeatMessage
 
   def getProducerConfig(engineType: String, configMap: java.util.Map[String, Object]): StreamJobProducerConfig
 
