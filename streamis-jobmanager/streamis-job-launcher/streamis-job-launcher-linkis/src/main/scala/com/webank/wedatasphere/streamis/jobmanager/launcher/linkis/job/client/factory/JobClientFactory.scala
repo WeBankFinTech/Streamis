ace2ac9ceb6d5e7cd245908f43c85a6bf43e21b4
jefftlin <jefft_lin@163.com>
2023-02-01 17:03:53 +0800

    Define the abstract relation of jobInfo

--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/client/factory/JobClientFactory.scala
+++ streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/client/factory/JobClientFactory.scala
@@ -1,13 +1,13 @@
 package com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.client.factory
 
 import com.webank.wedatasphere.streamis.jobmanager.launcher.job.manager.JobStateManager
-import com.webank.wedatasphere.streamis.jobmanager.launcher.job.{JobClient, LaunchJob}
-import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.{FlinkJobInfo, LinkisJobInfo}
+import com.webank.wedatasphere.streamis.jobmanager.launcher.job.{JobClient, JobInfo}
+import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.jobInfo.LinkisJobInfo
 import org.apache.linkis.computation.client.once.OnceJob
 
 trait JobClientFactory {
 
-  def createJobClient(onceJob: OnceJob, flinkJobInfo: FlinkJobInfo, jobStateManager: JobStateManager): JobClient[LinkisJobInfo]
+  def createJobClient(onceJob: OnceJob, jobInfo: JobInfo, jobStateManager: JobStateManager): JobClient[LinkisJobInfo]
 
   /**
    * Init the factory
