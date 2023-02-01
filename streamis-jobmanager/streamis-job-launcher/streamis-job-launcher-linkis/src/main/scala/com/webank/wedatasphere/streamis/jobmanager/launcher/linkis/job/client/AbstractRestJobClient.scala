02b6b9b10b1da50886620ad4f1150deba6a34162
jefftlin <jefft_lin@163.com>
2023-02-01 17:03:53 +0800

    Define the abstract relation of jobInfo

--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/client/AbstractRestJobClient.scala
+++ streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/client/AbstractRestJobClient.scala
@@ -1,14 +1,14 @@
 package com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.client
 
+import com.webank.wedatasphere.streamis.jobmanager.launcher.job.JobInfo
 import com.webank.wedatasphere.streamis.jobmanager.launcher.job.manager.JobStateManager
-import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.FlinkJobInfo
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.linkis.computation.client.once.OnceJob
 
 /**
  * @author jefftlin
  */
-abstract class AbstractRestJobClient(onceJob: OnceJob, jobInfo: FlinkJobInfo, stateManager: JobStateManager) extends AbstractJobClient(onceJob, jobInfo, stateManager) {
+abstract class AbstractRestJobClient(onceJob: OnceJob, jobInfo: JobInfo, stateManager: JobStateManager) extends AbstractJobClient(onceJob, jobInfo, stateManager) {
 
   /**
    * The http client in onceJob
