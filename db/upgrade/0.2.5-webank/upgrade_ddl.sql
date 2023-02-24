5bf4800a94b1f75203a8f2a77be5a9edc0eab2a7
jefftlin <jefft_lin@163.com>
2023-02-24 20:12:23 +0800

    update upgrade sql

--- db/upgrade/0.2.5-webank/upgrade_ddl.sql
+++ db/upgrade/0.2.5-webank/upgrade_ddl.sql
@@ -1,2 +1,2 @@
-alter table linkis_stream_job add column `current_version` varchar(50);
-alter table linkis_stream_job_version add column `manage_mode` varchar(30) default `EngineConn`;
+ALTER TABLE linkis_stream_job ADD COLUMN IF NOT EXISTS `current_version` varchar(50);
+ALTER TABLE linkis_stream_job_version ADD COLUMN IF NOT EXISTS `manage_mode` varchar(30) NOT NULL DEFAULT 'EngineConn';
