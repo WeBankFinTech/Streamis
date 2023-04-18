0650fc05519871406717fc210ffe37abf4d855d6
alexkun <xuanyu710@qq.com>
2023-04-18 21:55:50 +0800

    1. add support for flink detach engine.

--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-base/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/job/constants/JobConstants.scala
+++ streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-base/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/job/constants/JobConstants.scala
@@ -10,7 +10,20 @@ object JobConstants {
 
   val EC_INSTANCE_KEY = "ecInstance"
 
+  val EC_TICKET_ID_KEY = "ticketId"
+
   val MSG_KEY = "msg"
 
+  val APP_NAME_KEY = "applicationName"
+
+  val INSTANCE_KEY = "instance"
+
+  val SAVAPOINT_PATH_KEY = "savepointPath"
+
+  val MODE_KEY = "mode"
+
+  val RESULT_SAVEPOINT_PATH_KEY = "writtenSavepoint"
+
+  val RESULT_EC_METRICS_KEY = "ecMetrics"
 
 }
--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/manager/SimpleFlinkJobLaunchManager.scala
+++ streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-linkis/src/main/scala/com/webank/wedatasphere/streamis/jobmanager/launcher/linkis/job/manager/SimpleFlinkJobLaunchManager.scala
@@ -15,18 +15,26 @@
 
 package com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.manager
 
+import com.webank.wedatasphere.streamis.jobmanager.launcher.enums.JobClientType
+import com.webank.wedatasphere.streamis.jobmanager.launcher.job.constants.JobConstants
+import com.webank.wedatasphere.streamis.jobmanager.launcher.job.errorcode.JobLaunchErrorCode
 import com.webank.wedatasphere.streamis.jobmanager.launcher.job.state.{JobState, JobStateInfo}
-import com.webank.wedatasphere.streamis.jobmanager.launcher.job.{JobClient, LaunchJob}
+import com.webank.wedatasphere.streamis.jobmanager.launcher.job.{JobClient, JobInfo, LaunchJob}
 import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.conf.JobLauncherConfiguration
 import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.manager.SimpleFlinkJobLaunchManager.INSTANCE_NAME
 import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.jobInfo.LinkisJobInfo
 import com.webank.wedatasphere.streamis.jobmanager.launcher.linkis.job.jobInfo.EngineConnJobInfo
-import org.apache.commons.lang3.StringEscapeUtils
-import org.apache.linkis.common.utils.{RetryHandler, Utils}
-import org.apache.linkis.computation.client.once.simple.{SimpleOnceJob, SubmittableSimpleOnceJob}
+import org.apache.commons.lang3.{StringEscapeUtils, StringUtils}
+import org.apache.linkis.common.ServiceInstance
+import org.apache.linkis.common.exception.LinkisRetryException
+import org.apache.linkis.common.utils.{JsonUtils, RetryHandler, Utils}
+import org.apache.linkis.computation.client.once.action.GetEngineConnAction
+import org.apache.linkis.computation.client.once.simple.{SimpleOnceJob, SimpleOnceJobBuilder, SubmittableSimpleOnceJob}
 import org.apache.linkis.computation.client.once.{OnceJob, SubmittableOnceJob}
 import org.apache.linkis.computation.client.operator.impl.EngineConnApplicationInfoOperator
+import org.apache.linkis.governance.common.constant.ec.ECConstants
 import org.apache.linkis.httpclient.dws.DWSHttpClient
+import org.apache.linkis.protocol.utils.TaskUtils
 import org.apache.linkis.ujes.client.exception.UJESJobException
 
 import java.util
@@ -57,12 +65,16 @@ class SimpleFlinkJobLaunchManager extends FlinkJobLaunchManager {
     jobInfo.setName(StringEscapeUtils.escapeJava(job.getJobName))
     jobInfo.setId(onceJob.getId)
     jobInfo.setUser(job.getSubmitUser)
+    val startupMap = TaskUtils.getStartupMap(job.getParams)
+    val managerMode = startupMap.getOrDefault(JobLauncherConfiguration.MANAGER_MODE_KEY.getValue, JobClientType.ATTACH.getName).toString.toLowerCase()
+    jobInfo.setClientType(managerMode)
+    logger.info(s"Job manager mode : ${managerMode}")
     onceJob match {
       case simpleOnceJob: SubmittableSimpleOnceJob =>
         jobInfo.setECMInstance(simpleOnceJob.getECMServiceInstance)
       case _ =>
     }
-    Utils.tryCatch(fetchApplicationInfo(onceJob, jobInfo)) { t =>
+    Utils.tryCatch(fetchApplicationInfo(onceJob, jobInfo, JobClientType.DETACH.toString.equalsIgnoreCase(managerMode))) { t =>
       val message = s"Unable to fetch the application info of launched job [${job.getJobName}], maybe the engine has been shutdown"
       error(message, t)
       // Mark failed
@@ -81,7 +93,25 @@ class SimpleFlinkJobLaunchManager extends FlinkJobLaunchManager {
 
   override protected def createJobInfo(jobInfo: String): LinkisJobInfo = DWSHttpClient.jacksonJson.readValue(jobInfo, classOf[EngineConnJobInfo])
 
-  protected def fetchApplicationInfo(onceJob: OnceJob, jobInfo: EngineConnJobInfo): Unit = {
+  protected def fetchApplicationInfo(onceJob: OnceJob, jobInfo: EngineConnJobInfo, isDetach: Boolean): Unit = {
+    if (isDetach) {
+      val retryHandler = new RetryHandler {}
+      retryHandler.setRetryNum(JobLauncherConfiguration.FLINK_FETCH_APPLICATION_INFO_MAX_TIMES.getValue)
+      retryHandler.setRetryMaxPeriod(5000)
+      retryHandler.setRetryPeriod(500)
+      retryHandler.addRetryException(classOf[UJESJobException])
+      retryHandler.addRetryException(classOf[LinkisRetryException])
+      retryHandler.addRetryException(classOf[NullPointerException])
+      var ecInstance: ServiceInstance = null
+      var ecTicketId: String = null
+      onceJob match {
+        case submittableSimpleOnceJob: SubmittableSimpleOnceJob=>
+          ecInstance = submittableSimpleOnceJob.getEcServiceInstance
+          ecTicketId = submittableSimpleOnceJob.getEcTicketId
+        case _ =>
+      }
+      retryHandler.retry(getEcMetrics(ecTicketId, ecInstance, jobInfo), "GetEcMetrics")
+    } else {
     onceJob.getOperator(EngineConnApplicationInfoOperator.OPERATOR_NAME) match {
       case applicationInfoOperator: EngineConnApplicationInfoOperator =>
         val retryHandler = new RetryHandler {}
@@ -93,6 +123,37 @@ class SimpleFlinkJobLaunchManager extends FlinkJobLaunchManager {
         jobInfo.setApplicationId(applicationInfo.applicationId)
         jobInfo.setApplicationUrl(applicationInfo.applicationUrl)
     }
+  }
+
+  }
+
+
+  def getEcMetrics(ecTickecId: String, ecInstance: ServiceInstance, jobInfo: EngineConnJobInfo): Unit = {
+    val getEngingConnAction = new GetEngineConnAction
+    getEngingConnAction.setUser("hadoop")
+    if (StringUtils.isNotBlank(ecTickecId)) {
+      getEngingConnAction.addRequestPayload(JobConstants.EC_TICKET_ID_KEY, ecTickecId)
+    }
+    if (null != ecInstance) {
+      getEngingConnAction.addRequestPayload(JobConstants.APP_NAME_KEY, ecInstance.getApplicationName)
+      getEngingConnAction.addRequestPayload(JobConstants.INSTANCE_KEY, ecInstance.getInstance)
+    }
+    val rs = SimpleOnceJobBuilder.getLinkisManagerClient.getEngineConn(getEngingConnAction)
+    val metricsStr = rs.getData.getOrDefault(JobConstants.RESULT_EC_METRICS_KEY, "{}")
+    val metricsMap = if (null != metricsStr) {
+      JsonUtils.jackson.readValue(metricsStr.toString, classOf[util.Map[String, AnyRef]])
+    } else {
+      logger.info("metrics: \n" + JsonUtils.jackson.writeValueAsString(rs))
+      throw new LinkisRetryException(JobLaunchErrorCode.JOB_EC_METRICS_ERROR, "Got null metrics.")
+    }
+    val applicationId = if (metricsMap.containsKey(ECConstants.YARN_APPID_NAME_KEY)) {
+      metricsMap.get(ECConstants.YARN_APPID_NAME_KEY)
+    } else {
+      logger.info("metrics: \n" + JsonUtils.jackson.writeValueAsString(rs))
+      throw new LinkisRetryException(JobLaunchErrorCode.JOB_EC_METRICS_ERROR, "Got no appId.")
+    }
+    jobInfo.setApplicationId(applicationId.toString)
+    jobInfo.setApplicationUrl(metricsMap.getOrDefault(ECConstants.YARN_APP_URL_KEY, "").toString)
   }
 
 
