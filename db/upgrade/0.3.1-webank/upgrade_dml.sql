96d6bb3ea2f31bd6840d2a4c70f73a06b9533eeb
v_nikeyang <1013195908@qq.com>
2023-07-17 16:26:07 +0800

    appcon version

--- db/upgrade/0.3.1-webank/upgrade_dml.sql
+++ db/upgrade/0.3.1-webank/upgrade_dml.sql
@@ -1,4 +1,4 @@
-UPDATE bdp_easy_ide.linkis_stream_job_config_def
+UPDATE linkis_stream_job_config_def
 SET `key`='wds.linkis.flink.alert.failure.user', name='失败时告警用户', `type`='INPUT', sort=0, description='失败时告警用户', validate_type='None', validate_rule=NULL, `style`='', visiable=1, `level`=1, unit=NULL, default_value='', ref_values='', parent_ref=8, required=0, is_temp=0
 WHERE id=16;
 
