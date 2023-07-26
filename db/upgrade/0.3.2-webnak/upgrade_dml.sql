99528bf06e329b6e2d37fda27d044a9fdb581cc3
v_nikeyang <1013195908@qq.com>
2023-07-26 11:29:27 +0800

    add sql

---
+++ db/upgrade/0.3.2-webnak/upgrade_dml.sql
@@ -0,0 +1,2 @@
+INSERT INTO linkis_ps_error_code (error_code, error_desc, error_regex, error_type)
+VALUES('43042', '文件不存在: %s', 'Init executors error. Reason: FileNotFoundException: File (\\S+) does not exist', 0);
