f48b63028110ea30ff26a32ad4075bb1db58710b
alexkun <xuanyu710@qq.com>
2023-08-14 20:07:50 +0800

    1. merge upgrade sql

--- db/upgrade/0.2.5-webank/upgrade_dml.sql
+++
@@ -1,1 +0,0 @@
-UPDATE linkis_stream_job j SET current_version = (SELECT version FROM linkis_stream_job_version v WHERE v.job_id = j.id ORDER BY id DESC limit 1);
--- db/upgrade/0.3.0-webank/upgrade_ddl.sql
+++ db/upgrade/0.3.0-webank/upgrade_ddl.sql
@@ -1,2 +1,5 @@
+ALTER TABLE linkis_stream_job ADD COLUMN `current_version` varchar(50);
+ALTER TABLE linkis_stream_job_version ADD COLUMN IF NOT EXISTS `manage_mode` varchar(30) NOT NULL DEFAULT 'EngineConn';
+
 ALTER TABLE linkis_stream_project_files ADD update_time datetime NULL;
 ALTER TABLE linkis_stream_project_files ADD md5 varchar(100) NULL;
