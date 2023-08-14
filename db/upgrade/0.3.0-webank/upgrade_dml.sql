f8d4ec1fd593d672712245c77d39bad7dee34760
alexkun <xuanyu710@qq.com>
2023-08-14 20:07:50 +0800

    1. merge upgrade sql

--- db/upgrade/0.3.0-webank/upgrade_dml.sql
+++ db/upgrade/0.3.0-webank/upgrade_dml.sql
@@ -1,3 +1,5 @@
+UPDATE linkis_stream_job j SET current_version = (SELECT version FROM linkis_stream_job_version v WHERE v.job_id = j.id ORDER BY id DESC limit 1);
+
 UPDATE linkis_stream_job_config_def
 SET `key`='wds.linkis.flink.alert.failure.user', name='失败时告警用户', `type`='INPUT', sort=0, description='失败时告警用户', validate_type='None', validate_rule=NULL, `style`='', visiable=1, `level`=1, unit=NULL, default_value='', ref_values='', parent_ref=8, required=0, is_temp=0
 WHERE id=16;
