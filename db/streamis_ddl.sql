e99a5fda6121fd25aef05f6031a07636e29509dd
v_nikeyang <1013195908@qq.com>
2023-08-29 16:50:44 +0800

    update sql

--- db/streamis_ddl.sql
+++ db/streamis_ddl.sql
@@ -250,6 +250,18 @@ CREATE TABLE `linkis_stream_project_privilege` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='项目权限表';
 
 
+DROP TABLE IF EXISTS `linkis_stream_error_code`;
+
+CREATE TABLE `linkis_stream_error_code` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `error_code` varchar(50) COLLATE utf8_bin NOT NULL,
+  `error_desc` varchar(1024) COLLATE utf8_bin NOT NULL,
+  `error_regex` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `error_type` int(3) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=143 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
 ALTER TABLE `linkis_stream_project` ADD create_time datetime DEFAULT NULL;
 ALTER TABLE `linkis_stream_project` ADD last_update_by varchar(50) DEFAULT NULL;
 ALTER TABLE `linkis_stream_project` ADD last_update_time datetime DEFAULT NULL;
---
+++ db/upgrade/0.3.3-webank/upgrade_ddl.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `linkis_stream_error_code`;
+
+CREATE TABLE `linkis_stream_error_code` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `error_code` varchar(50) COLLATE utf8_bin NOT NULL,
+  `error_desc` varchar(1024) COLLATE utf8_bin NOT NULL,
+  `error_regex` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `error_type` int(3) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=143 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
