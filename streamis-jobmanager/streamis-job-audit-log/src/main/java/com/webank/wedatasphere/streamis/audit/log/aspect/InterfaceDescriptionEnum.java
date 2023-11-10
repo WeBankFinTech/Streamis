2975d89091b4c17548bbb430a377877837c2ee5a
v_nikeyang <1013195908@qq.com>
2023-11-10 18:08:26 +0800

    完善接口备注映射关系

--- streamis-jobmanager/streamis-job-audit-log/src/main/java/com/webank/wedatasphere/streamis/audit/log/aspect/InterfaceDescriptionEnum.java
+++ streamis-jobmanager/streamis-job-audit-log/src/main/java/com/webank/wedatasphere/streamis/audit/log/aspect/InterfaceDescriptionEnum.java
@@ -5,11 +5,28 @@ import java.util.Map;
 
 public enum InterfaceDescriptionEnum {
 
-    LIST("/list", "分页查找所有job"),
+    default34("/api/rest_j/v1/streamis/streamJobManager/config",""),
+    PROJECT_FILES_UPLOAD( "/api/rest_j/v1/streamis/streamProjectManager/project/files/upload", "上传工程资源文件"),
+    PROJECT_FILES_LIST("/api/rest_j/v1/streamis/streamProjectManager/project/files/list","查询工程资源文件"),
+    PROJECT_FILES_VERSION_LIST("/api/rest_j/v1/streamis/streamProjectManager/project/files/version/list","查询工程资源文件版本信息"),
+    PROJECT_FILES_DELETE("/api/rest_j/v1/streamis/streamProjectManager/project/files/delete","删除工程资源文件"),
+    PROJECT_FILES_VERSION_DELETE("/api/rest_j/v1/streamis/streamProjectManager/project/files/version/delete","删除工程资源文件版本"),
+    PROJECT_FILES_DOWNLOAD("/api/rest_j/v1/streamis/streamProjectManager/project/files/download","下载工程资源文件"),
+    JOB_BULK_EXECUTION("/api/rest_j/v1/streamis/streamJobManager/job/bulk/execution","job批量启动"),
+    JOB_BULK_PAUSE("/api/rest_j/v1/streamis/streamJobManager/job/bulk/pause","job批量停止"),
+    CONFIG_GET_WORKSPACE_USERS("/api/rest_j/v1/streamis/streamJobManager/config/getWorkspaceUsers","获取工作空间用户"),
+    CONFIG_DEFINITIONS("/api/rest_j/v1/streamis/streamJobManager/config/definitions","查询配置定义"),
+
     JOBCONTENT("/jobContent", "查看job详情");
 
     private String url;
     private String description;
+
+
+    private static  String systemPath = "/api/rest_j/v1";
+
+    private static  String projectManagerRestfulApiPath = "/streamis/streamProjectManager/project";
+
 
     InterfaceDescriptionEnum(String url, String description) {
         this.url = url;
