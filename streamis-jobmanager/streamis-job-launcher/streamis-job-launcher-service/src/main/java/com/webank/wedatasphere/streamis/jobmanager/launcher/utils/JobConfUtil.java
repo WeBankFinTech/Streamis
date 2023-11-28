35e58438c1aca20c1969166099f4a7384ba684a1
v_nikeyang <1013195908@qq.com>
2023-11-28 15:54:50 +0800

    if合并 默认的detach，要获取配置里的值

--- streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-service/src/main/java/com/webank/wedatasphere/streamis/jobmanager/launcher/utils/JobConfUtil.java
+++ streamis-jobmanager/streamis-job-launcher/streamis-job-launcher-service/src/main/java/com/webank/wedatasphere/streamis/jobmanager/launcher/utils/JobConfUtil.java
@@ -1,10 +1,12 @@
 package com.webank.wedatasphere.streamis.jobmanager.launcher.utils;
 
 import com.webank.wedatasphere.streamis.jobmanager.launcher.conf.JobConfKeyConstants;
+import com.webank.wedatasphere.streamis.jobmanager.launcher.conf.JobConstants;
 import com.webank.wedatasphere.streamis.jobmanager.launcher.entity.JobConfValue;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JobConfUtil {
 
@@ -12,15 +14,11 @@ public class JobConfUtil {
 
     public static List<JobConfValue> valuesHandler (List<JobConfValue> jobConfValueList){
         String manageMode = JobConfKeyConstants.MANAGE_MODE_KEY().getValue();
-        for (JobConfValue jobConfValue : jobConfValueList) {
-            if (jobConfValue.getKey().equals(manageMode)) {
-                if (jobConfValue.getValue().isEmpty()) {
-                    jobConfValue.setValue("detach");
-                    break;
-                }
-            }
-        }
-        return jobConfValueList;
+        List<JobConfValue> jobConfValues = jobConfValueList.stream()
+                .filter(jobConfValue -> jobConfValue.getKey().equals(manageMode) && jobConfValue.getValue().isEmpty())
+                .peek(jobConfValue -> jobConfValue.setValue(JobConstants.MANAGE_MODE_DETACH()))
+                .collect(Collectors.toList());
+        return jobConfValues;
     }
 
 
